fb895968db2e60a0866162e3f7e8f3a4935f65b2
Abakaxato <cauakika@gmail.com>
2026-08-18 11:14:39 -0300

    Reorganiznado novamente e inserindo resolução de exercicio

---
+++ Classes/ConversorDeMoedas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace OlaMundo.Classes
+{
+    internal static class ConversorDeMoedas
+    {
+        public static void Converter(double cotacao , double moeda) {
+            double valor = moeda * cotacao;
+            valor += (valor * 0.06);
+            Console.WriteLine("Valor a ser pago em reais = "+ valor.ToString("F2",CultureInfo.InvariantCulture));
+        }
+    }
+}
