f0f131cbae64723326c8b3826f3b31a3531056af
Abakaxato <cauakika@gmail.com>
2026-08-24 10:59:50 -0300

    Utilizando ref pra modificar o parametro passsado atraves de referencia na memoria

---
+++ Classes/testes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OlaMundo.Classes
+{
+    internal class testes
+    {
+
+        public int Triplicar(ref int x) {
+            x = x * 3;
+            return x;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,37 +6,13 @@ namespace OlaMundo
     {
         static void Main(string[] args)
         {
-            Console.Write("Digita o numero de produtos: ");
+            testes teste = new();
+            int a = 10;
 
-            int n = int.Parse(Console.ReadLine());
-            
-            Produto[] vetor = new Produto[n];
-            
-            Console.WriteLine($"{n} Produtos serão salvos, digite os valores de cada produto :");
-            
-            for (int i = 0; i < n; i++)
-            {
-                vetor[i] = new Produto();
-                Console.WriteLine($"\nProduto {i}:");
-                Console.Write("Digite o nome do produto: ");
-                vetor[i].Nome = Console.ReadLine();
-                Console.Write("Digite o Preco do produto: ");
-                vetor[i].Preco = double.Parse(Console.ReadLine());
-            }
-            for (int i = 0; i < n; i++)
-            {
-                Console.WriteLine(vetor[i].ToString());
-            }
+            teste.Triplicar(ref a);
 
-            double soma = 0;
-            double resultado = 0;
+            Console.WriteLine(a);
 
-            for (int i = 0; i < n; i++)
-            {
-                soma += vetor[i].Preco;
-            }
-            resultado = soma / n;
-            Console.WriteLine($"A média de preço dos produtos é {resultado.ToString("F2")}R$");
         }
     }
 }
