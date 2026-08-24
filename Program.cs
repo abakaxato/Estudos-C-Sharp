f4827f2ee956487bdf781e87a83add6a49135ece
Abakaxato <cauakika@gmail.com>
2026-08-24 13:36:12 -0300

    Finalizando começo das listas

--- Program.cs
+++ Program.cs
@@ -25,10 +25,10 @@ namespace OlaMundo
             {
                 Console.WriteLine(textos);
             }
-
+            Console.WriteLine("----------------------------------------------------------");
             //verificando o tamanho da lista 
             Console.WriteLine("tamanho da lista " + listaStrings.Count);
-
+            Console.WriteLine("----------------------------------------------------------");
             //procurando valores na lista
             string valorEncontrado = listaStrings.Find(x => x[0] == 'T');
             Console.WriteLine("valor encontrado no find : " + valorEncontrado);
@@ -38,10 +38,10 @@ namespace OlaMundo
             Console.WriteLine("----------------------------------------------------------");
             //fazendo uma busca no indice que atende os requisitos
             int valorNaPosição = listaStrings.FindIndex(x => x[0] == 'm'); //usando lambda
-            Console.WriteLine(valorNaPosição);
+            Console.WriteLine("Valor da posição pesquisada: " + valorNaPosição);
             Console.WriteLine("----------------------------------------------------------");
             int valorNaUltimaPosição = listaStrings.FindLastIndex(Predicate); // usando um predicato
-            Console.WriteLine(valorNaUltimaPosição);
+            Console.WriteLine("Valor da ultima posição pesquisada: " + valorNaUltimaPosição);
             Console.WriteLine("----------------------------------------------------------");
 
             //fazendo um filtro na lista
@@ -49,9 +49,42 @@ namespace OlaMundo
             listaFiltrada = listaStrings.FindAll(x => x.Length == 3);
             foreach (string elemento in listaFiltrada)
             {
-                Console.WriteLine(elemento);
+                Console.WriteLine("valor filtrado por 3 caracteres : " + elemento);
             }
             Console.WriteLine("----------------------------------------------------------");
+
+            //usando o remove
+            listaStrings.Remove("joao");
+            Console.WriteLine("lista sem o joao\n");
+            foreach (string textos in listaStrings)
+            {
+                Console.WriteLine(textos);
+            }
+            
+            Console.WriteLine("----------------------------------------------------------");
+            //usando o removeAll
+            listaStrings.RemoveAll(x => x.Length == 3);
+            Console.WriteLine("lista sem os valores de 3 caracteres\n");
+            foreach (string textos in listaStrings)
+            {
+                Console.WriteLine(textos);
+            }
+            Console.WriteLine("----------------------------------------------------------");
+            //usando o removeAt
+            listaStrings.RemoveAt(2);
+            Console.WriteLine("lista sem o valor na posição 2(terceiro)\n");
+            foreach (string textos in listaStrings)
+            {
+                Console.WriteLine(textos);
+            }
+            Console.WriteLine("----------------------------------------------------------");
+            //usando o removeRange
+            listaStrings.RemoveRange(1,2);
+            Console.WriteLine("lista sem 2 valores a partir da posição 1\n");
+            foreach (string textos in listaStrings)
+            {
+                Console.WriteLine(textos);
+            }
 
             //metodo usado no find quando não é utilizado o lambda
             static bool Predicate(string s)
