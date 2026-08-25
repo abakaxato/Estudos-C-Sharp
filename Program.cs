53fdd8a538e081d3b7d469542c7bf22fe203f719
Abakaxato <cauakika@gmail.com>
2026-08-25 10:33:14 -0300

    Aula 89 - Testando funções da string

--- Program.cs
+++ Program.cs
@@ -8,16 +8,38 @@ namespace OlaMundo
     {
         static void Main(string[] args)
         {
-            Program program = new Program();
-            int resultado = program.teste();
-            Console.WriteLine(resultado);
-        }
-        public int teste()
-        {
-            int x = int.Parse(Console.ReadLine());
-            int y = int.Parse(Console.ReadLine());
-            int z = 0;
-            return (x > y) ? 1 : 2;
+            string exemplo = "Esse é UM Exemplo de TEste UMA Vez Exemplo       ";
+            //Transformando a string toda em maiusculo
+            string maiusculo = exemplo.ToUpper();
+            Console.WriteLine(maiusculo+ " ToUpper");
+            //Transformando a string toda em minusculo
+            string minusculo = exemplo.ToLower();
+            Console.WriteLine(minusculo+ " ToLower");
+            //Removendo espaços da string
+            string trim = exemplo.Trim();
+            Console.WriteLine(trim+ " Trim");
+            //localizando primeiros valores na string
+            int localizacao = exemplo.IndexOf("UM");
+            Console.WriteLine("IndexOf de UM : " + localizacao);
+            //localizando ultimos valores na string
+            int ultimaLocalizacao = exemplo.LastIndexOf("UM");
+            Console.WriteLine("LastIndexOf de UM : " + ultimaLocalizacao);
+            //Recortando valores na string
+            string Recorte = exemplo.Substring(10,5);
+            Console.WriteLine("Substring : " + Recorte);
+            //Substituindo valores na string
+            string substituicao = exemplo.Replace('e','Z');
+            Console.WriteLine("substituicao : " + substituicao);
+            //Substituindo valores na string
+            string substituicao2 = exemplo.Replace("Exemplo", "AI IA AI IA AI IA");
+            Console.WriteLine("substituicao : " + substituicao2);
+            //Verificando se tem valores na string
+            bool verificacao = string.IsNullOrEmpty(exemplo);
+            Console.WriteLine("verificacao : " + verificacao);
+            //Verificando se tem valores validos na string
+            bool verificacao2 = string.IsNullOrWhiteSpace(exemplo);
+            Console.WriteLine("verificacao : " + verificacao2);
+
         }
     }
 }
