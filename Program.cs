4158a882b3a0c6cb94b87281c5d13b5f7a5fdf48
Abakaxato <cauakika@gmail.com>
2026-08-27 14:34:14 -0300

    testando datas

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using OlaMundo.Classes;
+using System.Globalization;
 using System.Threading.Channels;
 using static System.Net.Mime.MediaTypeNames;
 
@@ -8,38 +9,9 @@ namespace OlaMundo
     {
         static void Main(string[] args)
         {
-            string exemplo = "Esse é UM Exemplo de TEste UMA Vez Exemplo       ";
-            //Transformando a string toda em maiusculo
-            string maiusculo = exemplo.ToUpper();
-            Console.WriteLine(maiusculo+ " ToUpper");
-            //Transformando a string toda em minusculo
-            string minusculo = exemplo.ToLower();
-            Console.WriteLine(minusculo+ " ToLower");
-            //Removendo espaços da string
-            string trim = exemplo.Trim();
-            Console.WriteLine(trim+ " Trim");
-            //localizando primeiros valores na string
-            int localizacao = exemplo.IndexOf("UM");
-            Console.WriteLine("IndexOf de UM : " + localizacao);
-            //localizando ultimos valores na string
-            int ultimaLocalizacao = exemplo.LastIndexOf("UM");
-            Console.WriteLine("LastIndexOf de UM : " + ultimaLocalizacao);
-            //Recortando valores na string
-            string Recorte = exemplo.Substring(10,5);
-            Console.WriteLine("Substring : " + Recorte);
-            //Substituindo valores na string
-            string substituicao = exemplo.Replace('e','Z');
-            Console.WriteLine("substituicao : " + substituicao);
-            //Substituindo valores na string
-            string substituicao2 = exemplo.Replace("Exemplo", "AI IA AI IA AI IA");
-            Console.WriteLine("substituicao : " + substituicao2);
-            //Verificando se tem valores na string
-            bool verificacao = string.IsNullOrEmpty(exemplo);
-            Console.WriteLine("verificacao : " + verificacao);
-            //Verificando se tem valores validos na string
-            bool verificacao2 = string.IsNullOrWhiteSpace(exemplo);
-            Console.WriteLine("verificacao : " + verificacao2);
-
+            DateTime dateTime = new DateTime(2026,12,12,12,12,12,12);
+            dateTime = DateTime.ParseExact("2018-12-12 13:43:56", "yyyy-MM-dd HH:mm:s", CultureInfo.InvariantCulture);
+            Console.WriteLine(dateTime);
         }
     }
 }
