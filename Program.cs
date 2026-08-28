41a6e1f7a452738218e37c1eeacf197535183232
Abakaxato <cauakika@gmail.com>
2026-08-28 10:40:32 -0300

    DateTimeKind e padrão ISO 8601

--- Program.cs
+++ Program.cs
@@ -6,6 +6,16 @@ namespace OlaMundo
         static void Main(string[] args)
         {
             DateTime dateTime = new DateTime(2016,08,27,14,12,09);
+            DateTime dateTimeZ = DateTime.Parse("2016-08-27T14:12:09Z");
+            Console.WriteLine("Kind : " + dateTime.Kind);
+            Console.WriteLine("dateTime : " + dateTime);
+            Console.WriteLine("ToLocalTime : " + dateTime.ToLocalTime());
+            Console.WriteLine("ToUniversalTime : " + dateTime.ToUniversalTime());
+            Console.WriteLine("KindZ : " + dateTimeZ.Kind);
+            Console.WriteLine("dateTimeZ : " + dateTimeZ);
+            Console.WriteLine("ToLocalTimeZ : " + dateTimeZ.ToLocalTime());
+            Console.WriteLine("ToUniversalTimeZ : " + dateTimeZ.ToUniversalTime());
+            Console.WriteLine("--------------------------------------------");
             Console.WriteLine("ToLongDateString : " + dateTime.ToLongDateString());
             DateTime dateTime2 = new DateTime(2016,08,27,14,12,09);
             Console.WriteLine("ToLongTimeString : " + dateTime2.ToLongTimeString());
