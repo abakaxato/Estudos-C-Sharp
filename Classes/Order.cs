fba7c0d61483483d5a0e64c75236d76b0dc02314
Abakaxato <cauakika@gmail.com>
2026-08-28 12:34:00 -0300

    Estudos de enum

---
+++ Classes/Order.cs
@@ -0,0 +1,17 @@
+﻿using OlaMundo.Classes.Enums;
+
+namespace OlaMundo.Classes
+{
+    internal class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+
+        public OrderStatus Status{ get; set; }
+
+        public override string ToString()
+        {
+            return $"\nID :{Id}\nMoment:{Moment}\nOrder Status : {Status}";
+        }
+    }
+}
