70979051b0cc892f954e170bb655d289a06606e6
Abakaxato <cauakika@gmail.com>
2026-08-28 10:27:35 -0300

    deixando mais legivel

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
-﻿using OlaMundo.Classes;
-using System.Globalization;
-using System.Threading.Channels;
-using static System.Net.Mime.MediaTypeNames;
-
+﻿
 namespace OlaMundo
 {
     internal class Program
@@ -10,32 +6,36 @@ namespace OlaMundo
         static void Main(string[] args)
         {
             DateTime dateTime = new DateTime(2016,08,27,14,12,09);
-            Console.WriteLine(dateTime.ToLongDateString());
+            Console.WriteLine("ToLongDateString : " + dateTime.ToLongDateString());
             DateTime dateTime2 = new DateTime(2016,08,27,14,12,09);
-            Console.WriteLine(dateTime2.ToLongTimeString());
+            Console.WriteLine("ToLongTimeString : " + dateTime2.ToLongTimeString());
             DateTime dateTime3 = new DateTime(2016, 08, 27, 14, 12, 09);
-            Console.WriteLine(dateTime3.ToShortDateString());
+            Console.WriteLine("ToShortDateString : " + dateTime3.ToShortDateString());
             DateTime dateTime4 = new DateTime(2016, 08, 27, 14, 12, 09);
-            Console.WriteLine(dateTime4.ToString("yyyy-texto-MM-dd"));
+            Console.WriteLine("ToString : " + dateTime4.ToString("yyyy-texto-MM-dd"));
             Console.WriteLine("--------------------------------------------");
             DateTime dateTime5 = dateTime.AddHours(5);
-            Console.WriteLine(dateTime5);
+            Console.WriteLine("AddHours : " + dateTime5);
             DateTime dateTime6 = dateTime.AddTicks(900000000);
-            Console.WriteLine(dateTime6);
+            Console.WriteLine("AddTicks : " + dateTime6);
             Console.WriteLine("--------------------------------------------");
             TimeSpan diferenca = dateTime5.Subtract(dateTime6);
-            Console.WriteLine(diferenca);
+            Console.WriteLine("Subtract " + diferenca);
+            TimeSpan divisao = diferenca.Divide(2);
+            Console.WriteLine("Divide " + divisao);
             TimeSpan timeSpan = new TimeSpan(13,22,14);
-            Console.WriteLine(timeSpan.Ticks);
-            Console.WriteLine(timeSpan.Hours);
-            Console.WriteLine(timeSpan);
+            TimeSpan soma = diferenca.Add(timeSpan);
+            Console.WriteLine("Add " + soma);
+            Console.WriteLine("timeSpan.Ticks " + timeSpan.Ticks);
+            Console.WriteLine("timeSpan.Hours " + timeSpan.Hours);
+            Console.WriteLine("timeSpan " + timeSpan);
             Console.WriteLine("--------------------------------------------");
             TimeSpan timeSpan1 = TimeSpan.FromDays(2,23);
-            Console.WriteLine(timeSpan1);
-            Console.WriteLine(timeSpan1.Days);
+            Console.WriteLine("timeSpan " + timeSpan1);
+            Console.WriteLine("timeSpan.FromDays " + timeSpan1.Days);
             TimeSpan timeSpan2 = TimeSpan.FromHours(2,23);
-            Console.WriteLine(timeSpan2);
-            Console.WriteLine(timeSpan2.Days);
+            Console.WriteLine("timeSpan " + timeSpan2);
+            Console.WriteLine("timeSpan.Fromhours " + timeSpan2.Days);
         }
     }
 }
