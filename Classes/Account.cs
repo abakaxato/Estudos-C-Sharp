f206554c8a63377aecf350bc20d790ff776d5edf
Abakaxato <cauakika@gmail.com>
2026-08-30 17:57:35 -0300

    Estudo de herança aula 109

---
+++ Classes/Account.cs
@@ -0,0 +1,18 @@
+﻿namespace OlaMundo.Classes
+{
+    internal class Account
+    {
+        public int Number { get; protected set; }
+        public string Holder { get; protected set; } = string.Empty;
+        public double Balance { get; protected set; }
+        public Account()
+        {
+        }
+        public Account(int number,string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+    }
+}
---
+++ Classes/AccountBussines.cs
@@ -0,0 +1,24 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace OlaMundo.Classes
+{
+    internal class AccountBussines : Account
+    {
+        public double LoadLimit { get; set; }
+        public AccountBussines()
+        {
+        }
+        public AccountBussines(int number, string holder, double balance, double loadLimit)
+            : base(number, holder, balance)
+        {
+            LoadLimit = loadLimit;
+        }
+        public void Loan(double amount)
+        {
+            if (amount < LoadLimit) 
+            {
+                Balance += amount;
+            }
+        }
+    }
+}
