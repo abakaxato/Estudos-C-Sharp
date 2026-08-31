625d7f171febec33d628c183fe64772901702bcf
Abakaxato <cauakika@gmail.com>
2026-08-31 15:33:12 -0300

    testando try catch

--- Program.cs
+++ Program.cs
@@ -7,47 +7,28 @@ namespace OlaMundo
     {
         static void Main(string[] args)
         {
-            List<Pessoa> pessoas = new();
-            Console.Write("Enter the number os tax payers : ");
-            int n = int.Parse(Console.ReadLine());
-            for (int i = 1; i <= n; i++)
+            try
             {
-                Console.WriteLine($"Tax payer #{i} data");
-                Console.Write("Individual or company (i/c)? : ");
-                char e = char.Parse(Console.ReadLine());
-                Console.Write("Name : ");
-                string name = Console.ReadLine();
-                Console.Write("Years old : ");
-                int years = int.Parse(Console.ReadLine());
-                Console.Write("Anual income : ");
-                double income = double.Parse(Console.ReadLine());
-
-                switch (e){
-                    case 'i':
-                        Console.Write("health expenditures : ");
-                        double health = double.Parse(Console.ReadLine());
-                        pessoas.Add(new PessoaFisica(name,years,income,health));
-                        break;
-                    case 'c':
-                        Console.Write("Number of employees : ");
-                        int employeesNumber = int.Parse(Console.ReadLine());
-                        pessoas.Add(new PessoaJuridica(name,years,income,employeesNumber));
-                        break;
-                    default:
-                        Console.Write("Valor invalido");
-                        break;
-                }
-
+                int a, b;
+                a = int.Parse(Console.ReadLine());
+                b = int.Parse(Console.ReadLine());
+                int c = a / b;
+                Console.WriteLine(c);
             }
-            Console.WriteLine("\n\nTAXES PAID : ");
-            double totalTax= 0;
-            foreach (Pessoa pessoa in pessoas) 
+            catch (DivideByZeroException)
             {
-                totalTax += pessoa.CalcularImposto();
-                pessoa.PagarImposto();
-                Console.Write($"\n{pessoa}\n");
+                Console.WriteLine("Não pode dividir por zero");
             }
-            Console.WriteLine($"TOTAL TAXES :\nR${totalTax}");
+            catch (FormatException e)
+            {
+                Console.WriteLine("não pode colocar letras : " + e.Message);
+            }
+            finally 
+            {
+                Console.WriteLine("cabou");
+            }
+
+
         }
     }
 }
