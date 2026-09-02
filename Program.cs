9ff636fb88f01310965e62e481a37a996e3cc40a
Abakaxato <cauakika@gmail.com>
2026-09-02 20:11:51 -0300

    Estuando o Path

--- Program.cs
+++ Program.cs
@@ -7,6 +7,21 @@ namespace OlaMundo
     {
         static void Main(string[] args)
         {
+            string sourcePath = @"C:\temp\pasta com arquivos\subpasta com arquivos\arquivo1.txt";
+
+            //Estudando a classe Path
+            {
+                //Metodo para puxar o path completo de um path parcial
+                Console.WriteLine("GetFullPath : " + Path.GetFullPath(sourcePath));
+                //Metodo para puxar apenas o path
+                Console.WriteLine("GetDirectoryName : " + Path.GetDirectoryName(sourcePath));
+                //Metodo para puxar o nome do arquivo sem a extensão do arquivo
+                Console.WriteLine("GetFileNameWithoutExtension : " + Path.GetFileNameWithoutExtension(sourcePath));
+                //Metodo para puxar a extensão do arquivo
+                Console.WriteLine("GetExtension : " + Path.GetExtension(sourcePath));
+                //Metodo para puxar a pasta temporaria do meu sistema atual
+                Console.WriteLine("GetTempFileName : " + Path.GetTempFileName());
+            }
         }
     }
 }
