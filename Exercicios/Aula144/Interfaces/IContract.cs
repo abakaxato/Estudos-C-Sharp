34c3bfdc593b74a7096e6689f6663881b2c9a0fa
Abakaxato <cauakika@gmail.com>
2026-09-06 15:07:59 -0300

    Comecando o exercicio da aula 144

---
+++ Exercicios/Aula144/Interfaces/IContract.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OlaMundo.Exercicios.Aula144.Interfaces
+{
+    internal interface IContract
+    {
+        public string Name{ get; set; }
+        public DateTime InitialDate { get; set; }
+        public double ContratcValue { get; set; }
+        public List<string> InstallmentsList { get; set; }
+    }
+}
---
+++ Exercicios/Aula144/Service/Contract.cs
@@ -0,0 +1,40 @@
+﻿using OlaMundo.Exercicios.Aula144.Interfaces;
+using System.Globalization;
+namespace OlaMundo.Exercicios.Aula144.Service
+{
+    internal class Contract : IContract
+    {
+        public string Name { get; set; } = string.Empty;
+        public DateTime InitialDate { get; set; }
+        public double ContratcValue { get; set; }
+        public List<string> InstallmentsList { get; set; } = [];
+
+        public Contract(string name, DateTime date, double contractValue)
+        {
+            Name = name;
+            InitialDate = date;
+            ContratcValue = contractValue;
+        }
+        public List<string> CalcInstallments(Contract contract, int numberOfInstallments)
+        {
+            double monthlylValue = contract.ContratcValue / numberOfInstallments;
+            
+            for (int i = 0; i < numberOfInstallments; i++)
+            {
+                InstallmentsList.Add($"{InitialDate.AddMonths(i).ToString("dd/MM/yyyy")} - {monthlylValue * (i * 100)}");
+            }
+            return InstallmentsList;
+        }
+        public override string ToString()
+        {
+            string concat = string.Empty;
+
+            foreach(string installment in InstallmentsList)
+            {
+                concat += $"\n {installment}";
+            }
+            return $"Contract Name :{Name}\nContract Total Value : {ContratcValue.ToString("F2",CultureInfo.InvariantCulture)}\nInitial Date : {InitialDate.ToString("dd/MM/yyyy")}\nInstallments : {concat}";
+        }
+
+    }
+}
