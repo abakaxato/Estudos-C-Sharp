5f935e5af166c25efec101143941a4cd4404a8d1
Abakaxato <cauakika@gmail.com>
2026-09-13 14:57:28 -0300

    Extension Methods Aula 163

---
+++ Classes/Extensions Methods/StringExtension.cs
@@ -0,0 +1,27 @@
+﻿//Mudando o namespace para que ele fique no mesmo nivel do 
+// item que esta sendo extendido, assim deixando o método extendido
+// disponivel para todos que chamarem o seu dono
+namespace System
+{
+    static internal class StringExtension
+    {
+        //Extendendo um novo método para o objeto do tipo "string",
+        //agora onde eu criar uma string eu posso usar o método abaixo
+
+        //Método que retorna o antepenultimo valor em uma string
+        public static string ThirdToLastWord(this string thisObj,string split = " ") 
+        {
+            string[] text = thisObj.Split(split);
+
+            if (text.Length < 3)
+            {
+                return "this object is too small";
+            }
+            else
+            {
+                int wordPosition = text.Length - 3;
+                return text[wordPosition];
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,26 +3,6 @@
 
 #region Enunciado da questao
 /*
-Na contagem de votos de uma eleição, são gerados vários registros de votação contendo o nome do 
-candidato e a quantidade de votos (formato .csv) que ele obteve em uma urna de votação.
-Você deve fazer um programa para ler os registros de votação a partir de um arquivo, 
-e daí gerar um relatório consolidado com os totais de cada candidato.
-
-Dados que eu usei :
-
-Alex Green,21
-Maria Brown,30
-Bob Blue,15
-Alex Green,15
-Maria Brown,25
-Alex Green,22
-Bob Blue,10
-Maria Brown,35
-Alex Green,31
-Bob Blue,8
-
-
-Caminho : C:\temp\in.csv
 
 */
 #endregion Enunciado da questao
@@ -38,28 +18,12 @@ namespace OlaMundo
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter file full path : ");
-            string path = Path.GetFullPath(Console.ReadLine() ?? "");
-            string[] lines = File.ReadAllLines(path);
-            Dictionary<string, int> candidatedXVote = new();
-            int totalLines = lines.Length;
-            List<string> splitLine = new();
-            for (int i = 0; i < totalLines; i++)
-            {
-                string[] candidate = lines[i].Split(",");
-                if (candidatedXVote.ContainsKey(candidate[0]))
-                {
-                    candidatedXVote[candidate[0]] += int.Parse(candidate[1]);
-                }
-                else
-                {
-                    candidatedXVote[candidate[0]] = int.Parse(candidate[1]);
-                } 
-            }
-            foreach (KeyValuePair<string, int> keyValuePair in candidatedXVote)
-            {
-                Console.WriteLine($"Candidated : {keyValuePair.Key} Total Votes : {keyValuePair.Value}");
-            }
+            string text = "Criando";
+            Console.WriteLine(text.ThirdToLastWord());
+            string text2 = "Criando.e.usando.métodos.extendidos.comstring.e.utilizando.pontos.para.divisao";
+            Console.WriteLine(text2.ThirdToLastWord("."));
+            string text3 = "Criando e usando métodos extendidos com string";
+            Console.WriteLine(text3.ThirdToLastWord());
         }
     }
 }
