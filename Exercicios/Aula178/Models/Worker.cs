a6d7de2880a0e230cf17c56844a30876d32e5398
Abakaxato <cauakika@gmail.com>
2026-09-15 20:46:05 -0300

    Solução do exercicio da aula 178 (Ultima Aula)

---
+++ Exercicios/Aula178/Models/Worker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OlaMundo.Exercicios.Aula178.Models
+{
+    internal class Worker(string name = "", string email = "",double wage = 0)
+    {
+        public string Name { get; set; } = name;
+        public string Email { get; set; } = email;
+        public double Wage { get; set; } = wage;
+        public override string ToString()
+        {
+            return $"Nome : {Name} | Email : | {Email} Salario : {Wage}";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,14 +1,27 @@
-﻿//Exercicio da aula 159
+﻿//Exercicio da aula 178
 
 
 #region Enunciado da questao
 /*
+Fazer um programa para ler os dados (nome, email e salário) de funcionários a partir de um arquivo em formato .csv.  
+Em seguida mostrar, em ordem alfabética, o email dos funcionários cujo salário seja superior a um dado valor fornecido pelo usuário.  
+Mostrar também a soma dos salários dos funcionários cujo nome começa com a letra 'M'.
 
-*/
+Começo da lista no arquivo.csv :
+
+Maria,maria@gmail.com,3200.00
+Alex,alex@gmail.com,1900.00
+Marco,marco@gmail.com,1700.00
+Bob,bob@gmail.com,3500.00
+Anna,anna@gmail.com,2800.00
+
+path padrão :
+C:\temp\in.csv
+ */
 #endregion Enunciado da questao
 
-using OlaMundo.Exercicios.Aula116;
-using System.Threading.Channels;
+using OlaMundo.Exercicios.Aula178.Models;
+using System.Globalization;
 
 namespace OlaMundo
 {
@@ -16,7 +29,39 @@ namespace OlaMundo
     {
         static void Main(string[] args)
         {
-           
+            Console.Write("Digite o path do arquivo.csv : ");
+            string prePath = Console.ReadLine();
+            string path = Path.GetFullPath(prePath);
+
+            List<Worker> workers = new();
+
+            using (StreamReader reader = new(path))
+            {
+                while (!reader.EndOfStream)
+                { 
+                   string[] split = reader.ReadLine().Split(",");
+                   workers.Add(new Worker(split[0], split[1], double.Parse(split[2])));
+                }
+            }
+            
+            Console.WriteLine("\nEsses são os funcionarios que estão cadastrados : \n");
+            workers.ForEach(Console.WriteLine);
+
+            Console.Write("\n Qual é o valor minimo que você quer usar para filtrar os funcionarios ? ");
+            double minimumWage = double.Parse(Console.ReadLine());
+
+            Console.Write("\n Quer adicionar um filtro aos e-mails ? digite o texto ou para filtrar ou clique em enter para ver todos : ");
+            string filterEmail = Console.ReadLine() ?? "@";
+
+            var filtredWorkers = workers
+                .Where(worker => worker.Wage > minimumWage && (worker.Email.Split("@")[0].Contains(filterEmail)))
+                .OrderBy(workers => workers.Email).ToList();
+
+            Console.WriteLine($"\nEsse são os funcionarios que recebem acima de R${minimumWage.ToString("F2",CultureInfo.InvariantCulture)} e estão dentro do seu filtro '{filterEmail}': \n");
+            filtredWorkers.ForEach(Console.WriteLine);
+
+            var sumOfFilteredWages = filtredWorkers.Sum(worker => worker.Wage);
+            Console.WriteLine($"\n Esse é o valor total somando os salarios de todos os funcionarios acima : {sumOfFilteredWages.ToString("F2",CultureInfo.InvariantCulture)}");
         }
     }
 }
